476f06f29c1faf5163f9d31b30021138d0780980
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-11-30 01:11:03 +0200

    Added more worked with companies

--- src/components/Loop.jsx
+++ src/components/Loop.jsx
@@ -3,9 +3,19 @@ import LogoLoop from './LoopCode'
 
 const logos = [
   {
+    src: "https://res.cloudinary.com/dkmzveqce/image/upload/v1764445756/ChatGPT_Image_Nov_29_2025_09_42_44_PM_k8xeji.png",
+    alt: "NISSAN",
+    href: "https://nissan.com",
+  },
+  {
     src: "https://res.cloudinary.com/dkmzveqce/image/upload/v1763639424/ChatGPT_Image_Nov_20_2025_11_03_16_AM_nu4g9r.png",
     alt: "JAC Motors",
     href: "https://jacmotors.com",
+  },
+  {
+    src: "https://res.cloudinary.com/dkmzveqce/image/upload/v1764444650/ChatGPT_Image_Nov_25_2025_09_30_45_PM_zgaywe.png",
+    alt: "TMM",
+    href: "https://tmm.com",
   },
 //   {
 //     src: "https://res.cloudinary.com/dkmzveqce/image/upload/v1763639422/ChatGPT_Image_Nov_20_2025_11_19_34_AM_dcxskm.png",
@@ -17,6 +27,21 @@ const logos = [
     src: "https://res.cloudinary.com/dkmzveqce/image/upload/v1763643107/ChatGPT_Image_Nov_20_2025_02_48_32_PM_mfd2bt.png",
     alt: "BYD",
     href: "https://byd.com",
+  },
+  {
+    src: "https://res.cloudinary.com/dkmzveqce/image/upload/v1764447191/ChatGPT_Image_Nov_29_2025_10_10_13_PM_bg0qtl.png",
+    alt: "Europcar",
+    href: "https://Europcar.com",
+  },
+  {
+    src: "https://res.cloudinary.com/dkmzveqce/image/upload/v1764448224/ChatGPT_Image_Nov_29_2025_10_28_22_PM_wegvvk.png",
+    alt: "Salvino Del",
+    href: "https://Salvino Del.com",
+  },
+  {
+    src: "https://res.cloudinary.com/dkmzveqce/image/upload/v1764444640/ChatGPT_Image_Nov_25_2025_10_48_31_PM_xgzcsk.png",
+    alt: "GlassFit",
+    href: "https://glassfit.com",
   },
 ]
 
